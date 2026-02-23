25c7748b6b32d21e15c33b122d6c54f2d63be16d
Harshit Singh <jabincreators@gmail.com>
2026-02-23 16:39:35 +0530

    fixed logos

--- app/(public)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react'
-import ClientLayout from '@/components/ClientLayout'
-
-export default function PublicLayout({ children }: { children: ReactNode }) {
-  return <ClientLayout>{children}</ClientLayout>
-}
